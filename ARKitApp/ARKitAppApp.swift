f0ef36a6f2eab2c9448b352941b483842aab095f
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2024-04-11 11:15:56 -0400

    setting up app

---
+++ ARKitApp/ARKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKitAppApp.swift
+//  ARKitApp
+//
+//  Created by asmaa gamal  on 11/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ARKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARKitApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARKitApp
+//
+//  Created by asmaa gamal  on 11/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
